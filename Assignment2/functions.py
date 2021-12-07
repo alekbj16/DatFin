faf738404e044790934a42cd0a72100024b904a1
aleksbj96@gmail.com <aleksbj96@gmail.com>
2021-12-07 15:29:38 +0100

    efficient frontier plotted using real data

--- Assignment2/functions.py
+++ Assignment2/functions.py
@@ -1,5 +1,7 @@
 import numpy as np
+from numpy.core.fromnumeric import var
 from numpy.lib.twodim_base import diag 
+import pandas_datareader as web
 
 def optimal_portofolio(r_f,r_1,r_2,s_1,s_2,rho):
     correlation_matrix = np.zeros((2,2))
@@ -29,4 +31,93 @@ def portofolio_variance(r_1,r_2,s_1,s_2,X_opt,rho):
 def get_slope(r_f,portofolio_return,portofolio_variance):
     return (portofolio_return-r_f)/portofolio_variance
 
+def get_price_data(tickers, start, end,interval,adj_close=False):
+    """
+    Input:
+        Ticker: Uppercase ticker symbol
+        Start, end: datetime object
+        Interval: "y","m","d","h"
+        Adj_close: Gives only adjusted close price if True. 
+    Output: List of dataframes
+    """
+    data = []
+    for t in tickers:
+        data.append(web.get_data_yahoo(t,start,end,interval=interval))
+    if adj_close==True:
+        data_adj_closed = []
+        for d in data:
+            adj_closed = d[['Adj Close']].copy()
+            data_adj_closed.append(adj_closed)
+        return data_adj_closed
+    return data
 
+def portofolio_strategies():
+    x1 = np.linspace(0.0,1.0,11)
+    X = [1-x1,x1]
+    return np.array(X)
+
+# def calculate_returns(strategies,r_1,r_2):
+#     returns = []
+#     for i in range(0,len(strategies[0])):
+#         returns.append(strategies[0][i]*r_1+strategies[1][i]*r_2)
+#     return np.array(returns)
+
+def calculate_variances(strategies,r_1,r_2,s_1,s_2,rho):
+    variances = []
+    for i in range(0,len(strategies[0])):
+        variances.append((strategies[0][i]**2)*(s_1**2)+(strategies[1][i]**2)*(s_2**2)+2*strategies[0][i]*strategies[0][i]*s_1*s_2*rho)
+    return np.array(variances)
+
+def mean_to_var(means,vars):
+    return np.divide(means,vars)
+
+
+def calculate_returns(data):
+    """
+    Input: 
+        data: dataframe with historical prices
+    Output:
+        returns for Adjusted Close price by formula R = log(P_t/P_(t-1))
+    """
+    returns = []
+    for i in range(0,len(data)):
+        if i == len(data)-1:
+            break
+        r = np.log(float(data['Adj Close'][i+1])/float(data['Adj Close'][i]))
+        returns.append(r)
+    return returns
+
+def yearly_returns(weekly_returns):
+    """
+    Input: 
+        List of weekly returns as obtained by the 'calculate_returns' function
+    Output:
+        Yearly returns (sum of daily)
+    Note: Assumes 52 weeks per year
+    """
+    yr_returns = np.add.reduceat(weekly_returns,np.arange(0,len(weekly_returns),52))
+    return yr_returns
+
+def covariance_matrix(data):
+    """
+    Input:
+        data is array on form [yrly_mcd,yrly_ko,yrly_msft]
+    Output:
+        Corvariance matrix as described in assignment
+    """
+    
+    #Variance of returns
+    var_returns = np.array([np.var(data[0]),np.var(data[1]),np.var(data[2])])
+    s = np.zeros((3,3))
+    #Covar coefficients
+    s_mcd_ko = np.corrcoef(np.array((data[0],data[1])))[0,1]
+    s_mcd_msft = np.corrcoef(np.array((data[0],data[2])))[0,1]
+    s_ko_msft = np.corrcoef(np.array((data[1],data[2])))[0,1]
+    s[0,1] = s_mcd_ko
+    s[1,0] = s_mcd_ko
+    s[0,2] = s_mcd_msft
+    s[2,0] = s_mcd_msft
+    s[1,2] = s_ko_msft
+    s[2,1] = s_ko_msft
+    np.fill_diagonal(s,var_returns)
+    return s
--- Assignment2/main.py
+++ Assignment2/main.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 plt.style.use("seaborn")
 from numpy.lib.twodim_base import diag
 from  functions import * 
+import datetime as dt
 
 #***1: Efficient frontier with and withoud riskless lending and borrowing***
-
 r_f = 0.02 #Risk free rate: 2%
 r_1 = 0.10 #Asset 1 yearly expected return: 10%
 s_1 = 0.10 #Asset 1 standard dev
@@ -15,37 +15,114 @@ s_2 = 0.20 #Asset 2 standard dev
 
 corr_coeff = np.array([1,0.5,0,-1]) #The different correlation coefficients between asset 1 and 2
 
-
 #***1.1: How to plot efficient frontier *with* borrowing
 
 #****1.1.1: 
 #For the case rho = 0.5
-
 rho = corr_coeff[1]
 opt_portofolio = optimal_portofolio(r_f=r_f,r_1=r_1,r_2=r_2,s_1=s_1,s_2=s_2,rho=rho)
 opt_portofolio_returns = portofolio_return(r_1=r_1,r_2=r_2,X_opt=opt_portofolio)
 opt_portofolio_var = portofolio_variance(r_1,r_2,s_1=s_1,s_2=s_2,X_opt=opt_portofolio,rho=rho)
 
 
+
 x = np.linspace(0,0.04,10)
-r_f = np.linspace(0.02,0.08,4)
-for i in r_f:
-    slope = get_slope(r_f=i,portofolio_return=opt_portofolio_returns,portofolio_variance=opt_portofolio_var)
-    f_x = slope*x+i
-    plt.plot(x,f_x,label=f"r_f = {i}")
-plt.title(f"\u03C1 = {rho}")
+#r_f = np.linspace(0.02,0.08,4)
+slope = get_slope(r_f=r_f,portofolio_return=opt_portofolio_returns,portofolio_variance=opt_portofolio_var)
+f_x = slope*x+r_f
+plt.plot(x,f_x,label=f"r_f = {r_f}")
+plt.title(f"Efficient frontier with \u03C1 = {rho}")
 plt.legend()
-plt.xlabel("Standard deviation")
+plt.xlabel("Variance")
 plt.ylabel("Expected return")
 plt.show()
-# Xopt is the point which has highest slope and 
-# describes the optimal amount of stock
 
-#print(correlation_matrix)
-#print(omega)
-
+#Without borrowing: 
+#Adapting procedure as described in the book [p. 100]:
+#***
 # Assume that a riskless lending and borrowing rate exists
 # and find the optimum portfolio. Then assume that a different riskless lending and borrowing
 # rate exists and find the optimum portfolio that corresponds to this second rate.
 # Continue changing the assumed riskless rate until the full efficient frontier is determined.
+#***
 
+r_fs = np.array([0.02,0.04,0.06,0.08])
+plt.figure()
+for r_f in r_fs:
+    opt_portofolio = optimal_portofolio(r_f=r_f,r_1=r_1,r_2=r_2,s_1=s_1,s_2=s_2,rho=rho)
+    opt_portofolio_returns = portofolio_return(r_1=r_1,r_2=r_2,X_opt=opt_portofolio)
+    opt_portofolio_var = portofolio_variance(r_1,r_2,s_1=s_1,s_2=s_2,X_opt=opt_portofolio,rho=rho)
+    x = np.linspace(0,0.03,10)
+    slope = get_slope(r_f=r_f,portofolio_return=opt_portofolio_returns,portofolio_variance=opt_portofolio_var)
+    f_x = slope*x+r_f
+    plt.plot(x,f_x,label=f"r_f = {r_f}")
+    plt.title(f"Efficient frontier, no lending and borrowing, with \u03C1 = {rho}")
+plt.legend()
+plt.xlabel("Standard deviation")
+plt.ylabel("Variance")
+plt.show()
+
+
+###***With actual data***###
+
+#***Data collection***
+tickers = ["PEP","KO","MSFT"] #Pepsi, Coca-Cola, Microsoft
+
+#10 years of data
+start = dt.datetime(1991,1,1)
+end = dt.datetime(2001,1,1)
+data = get_price_data(tickers=tickers,start=start,end=end,interval="w",adj_close=True)
+data_pep = data[0]
+data_ko = data[1]
+data_msft = data[2]
+
+returns_pep = calculate_returns(data_pep)
+returns_ko = calculate_returns(data_ko)
+returns_msft = calculate_returns(data_msft)
+
+#***3: Mean and covariance matrix of yearly returns***
+yrly_pep = yearly_returns(returns_pep)
+yrly_ko = yearly_returns(returns_ko)
+yrly_msft = yearly_returns(returns_msft)
+
+mean_returns = np.array([np.mean(yrly_pep),np.mean(yrly_ko),np.mean(yrly_msft)]) #Mean of yearly returns
+covar_mat = covariance_matrix([yrly_pep,yrly_ko,yrly_msft])
+
+
+
+#***1: Efficient frontier with and withoud riskless lending and borrowing***
+r_f = 0.02 #Risk free rate: 2%
+
+#****2.1: 
+#For the case rho = 0.5
+rho = corr_coeff[1]
+opt_portofolio = optimal_portofolio(r_f=r_f,r_1=mean_returns[0],r_2=mean_returns[1],s_1=np.sqrt(covar_mat[0][0]),s_2=np.sqrt(covar_mat[1][1]),rho=rho)
+opt_portofolio_returns = portofolio_return(r_1=mean_returns[0],r_2=mean_returns[1],X_opt=opt_portofolio)
+opt_portofolio_var = portofolio_variance(r_1=mean_returns[0],r_2=mean_returns[1],s_1=np.sqrt(covar_mat[0][0]),s_2=np.sqrt(covar_mat[1][1]),X_opt=opt_portofolio,rho=rho)
+
+x = np.linspace(0,0.04,10)
+#r_f = np.linspace(0.02,0.08,4)
+slope = get_slope(r_f=r_f,portofolio_return=opt_portofolio_returns,portofolio_variance=opt_portofolio_var)
+f_x = slope*x+r_f
+plt.plot(x,f_x,label=f"r_f = {r_f}")
+plt.title(f"Efficient frontier with \u03C1 = {rho} and real data")
+plt.legend()
+plt.xlabel("Variance")
+plt.ylabel("Expected return")
+plt.show()
+
+r_fs = np.array([0.02,0.04,0.06,0.08])
+plt.figure()
+for r_f in r_fs:
+    opt_portofolio = optimal_portofolio(r_f=r_f,r_1=mean_returns[0],r_2=mean_returns[1],s_1=np.sqrt(covar_mat[0][0]),s_2=np.sqrt(covar_mat[1][1]),rho=rho)
+    opt_portofolio_returns = portofolio_return(r_1=mean_returns[0],r_2=mean_returns[1],X_opt=opt_portofolio)
+    opt_portofolio_var = portofolio_variance(r_1=mean_returns[0],r_2=mean_returns[1],s_1=np.sqrt(covar_mat[0][0]),s_2=np.sqrt(covar_mat[1][1]),X_opt=opt_portofolio,rho=rho)
+    x = np.linspace(0,0.03,10)
+    slope = get_slope(r_f=r_f,portofolio_return=opt_portofolio_returns,portofolio_variance=opt_portofolio_var)
+    f_x = slope*x+r_f
+    plt.plot(x,f_x,label=f"r_f = {r_f}")
+    plt.title(f"Efficient frontier, no lending and borrowing, with \u03C1 = {rho}. Using real data")
+plt.legend()
+plt.xlabel("Standard deviation")
+plt.ylabel("Variance")
+plt.show()
